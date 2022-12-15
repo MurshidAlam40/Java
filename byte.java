aa01c85d4d4c75f06216c07798ad2bc101b84cda
Murshid Alam <104639318+MurshidAlam40@users.noreply.github.com>
2022-12-15 18:35:12 +0530

    Create byte.java

---
+++ byte.java
@@ -0,0 +1,10 @@
+class ByteCode {
+    public static void main(String[] args) {
+        byte a=126;
+        a++;
+        a++;
+        System.out.println(a+" ");
+        a++;
+        System.out.println(a);
+    }
+}
