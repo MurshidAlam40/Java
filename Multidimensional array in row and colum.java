a659bf6ea4c638ed723b656789fd7870c7444593
Murshid Alam <104639318+MurshidAlam40@users.noreply.github.com>
2022-12-31 17:13:58 +0530

    Create Multidimensional array in row and colum.java

---
+++ Multidimensional array in row and colum.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+
+class GFG 
+{ 
+
+	public static void main(String args[]) 
+    {
+        int m = 2, n = 3;
+        
+    	int arr[][] = new int[m][n];
+
+    	for(int i = 0; i < arr.length; i++)
+    	{
+    		for(int j = 0; j < arr[i].length; j++)
+    		{
+    		    arr[i][j] = 10;
+    		    
+    			System.out.print(arr[i][j] + " ");
+    		}
+    	}
+    } 
+
+}
+
+
+//line change
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+
+class GFG 
+{ 
+
+	public static void main(String args[]) 
+    {
+        int m = 2, n = 3;
+        
+    	int arr[][] = new int[m][n];
+
+    	for(int i = 0; i < arr.length; i++)
+    	{
+    		for(int j = 0; j < arr[i].length; j++)
+    		{
+    		    arr[i][j] = 10;
+    		    
+    			System.out.print(arr[i][j] + " ");
+    		}
+    		
+    		System.out.println();//line change
+    	}
+    } 
+
+}
