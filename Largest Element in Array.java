54924b1e50ab48af7030a5fda38bad0541d98e7a
Murshid Alam <104639318+MurshidAlam40@users.noreply.github.com>
2023-01-01 18:49:12 +0530

    Create Largest Element in Array.java
    Input:
    n = 5
    A[] = {1, 8, 7, 56, 90}
    Output:
    90
    Explanation:
    The largest element of given array is 90.

---
+++ Largest Element in Array.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.largest(a, n));
+            
+        }
+	}
+}
+
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        Arrays.sort(arr);
+        return arr[n - 1];
+    }
+}
+*/
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        //int i;
+         //int max = arr[0];
+        int max = Integer.MIN_VALUE;
+        for (int i=0; i<n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
