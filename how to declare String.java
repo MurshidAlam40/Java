709e59be725401833021297ebe5add013147804b
Murshid Alam <104639318+MurshidAlam40@users.noreply.github.com>
2023-01-02 12:59:30 +0530

    Create how to declare String.java
    Output:
    String s = MurshidAlam
    String s1 = MurshidAlam

---
+++ how to declare String.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.*;
+
+class Test {
+    public static void main(String[] args)
+    {
+        // Declare String without using new operator
+        String s = "MurshidAlam";
+
+        // Prints the String.
+        System.out.println("String s = " + s);
+
+        // Declare String using new operator
+        String s1 = new String("MurshidAlam");
+
+        // Prints the String.
+        System.out.println("String s1 = " + s1);
+    }
+}
