842251183d003899eb09876df1f8323cc456d0d7
Murshid Alam <104639318+MurshidAlam40@users.noreply.github.com>
2023-01-11 16:31:29 +0530

    Create Find Transition Point.java
    Given a sorted array containing only 0s and 1s, find the transition point. 
    
    
    Example 1:
    Input:
    N = 5
    arr[] = {0,0,0,1,1}
    Output: 3
    Explanation: index 3 is the transition 
    the point where 1 begins.

---
+++ Find Transition Point.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Sorted_Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        GfG obj = new GfG();
+        System.out.println(obj.transitionPoint(arr, n));
+    }
+}
+class GfG {
+    int transitionPoint(int arr[], int n) {
+        // code here
+        
+        for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
