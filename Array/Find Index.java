87ffd6ced570970c36551b050339770ab4a83455
Murshid Alam <104639318+MurshidAlam40@users.noreply.github.com>
2023-03-22 21:35:58 +0530

    Create Find Index.java
    Input:
    N = 6
    arr[] = { 1, 2, 3, 4, 5, 5 }
    Key = 5
    Output:  4 5
    Explanation:
    5 appears first time at index 4 and
    appears last time at index 5.
    (0 based indexing)

---
+++ Array/Find Index.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+
+class Array
+ {
+	public static void main (String[] args){
+	 
+	    Scanner sc=new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int key = sc.nextInt();
+        Solution ob=new Solution();
+        int ar[]=ob.findIndex(arr,n,key);
+        System.out.println(ar[0]+" "+ar[1]);
+	}
+}
+
+
+class Solution
+{ 
+    // Function to find starting and end index 
+    static int[] findIndex(int a[], int N, int key) 
+    { 
+        //code here.
+        int index[] = {-1, -1};
+        
+        for(int i=0; i<N; i++){
+            if(a[i] == key){
+                index[0] = i;
+                break;
+            }
+        }
+        for(int i=N-1; i>=0; i--){
+            if(a[i] == key){
+                index[1] = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
